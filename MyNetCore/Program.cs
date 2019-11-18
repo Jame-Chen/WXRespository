44811c14a5e1980b17efbc58ca478838b50f97b8
Jame-Chen <931863353@qq.com>
2019-11-18 09:22:14 +0800

    update

--- MyNetCore/Program.cs
+++ MyNetCore/Program.cs
@@ -19,6 +19,7 @@ namespace MyNetCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                 .UseUrls("http://*:5000")
                  .UseKestrel()
                  .UseIIS()
                  .UseIISIntegration()
