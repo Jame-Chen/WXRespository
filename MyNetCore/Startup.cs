247c32994efc4b87a35aafe068342cd5c4614887
Jame-Chen <931863353@qq.com>
2019-11-29 10:02:02 +0800

    update

--- MyNetCore/Startup.cs
+++ MyNetCore/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -16,19 +15,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using MyNetCore.Middleware;
 using Sys.Reponsitory;
-using Sys.Reponsitory.Core;
 using Sys.Service;
 using Microsoft.OpenApi.Models;
 using System.IO;
-using Sys.Reponsitory.Interface;
-using Microsoft.Extensions.Logging;
 using MyNetCore.Filter;
 using log4net;
 using log4net.Config;
 using log4net.Repository;
-using MySql;
 using Microsoft.AspNetCore.HttpOverrides;
-using System.Net;
 
 namespace MyNetCore
 {
@@ -178,7 +172,7 @@ namespace MyNetCore
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                 c.RoutePrefix = string.Empty;
             });
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseMvc();
